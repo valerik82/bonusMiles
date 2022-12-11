d453b08935a9ac90a1054134e6568f74e9e91630
Valeriya Masarsky <assidora@gmail.com>
2022-12-11 16:25:05 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int oneMile = 20; // amount of Rubles for 1 mile
+        int ticketPrice = 2100; // ticket price
+        int bonusMiles = ticketPrice / oneMile; //amount of miles per ticket
+
+        System.out.println(bonusMiles);
+    }
+}
